2ddc34e5e9c385cc5288f9f462ffd914fbc48ce1
Domitrii <126395589+Domitrii@users.noreply.github.com>
2024-02-05 16:05:35 +0000

    Deploying to gh-pages from @ Domitrii/goit-js-hw-012@4f3d87c14a73933ed4bbf1706ae39c5fb9a4b1fb 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{S as w,i as M}from"./assets/vendor-46aac873.js";import"https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const l of o.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&i(l)}).observe(document,{childList:!0,subtree:!0});function n(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerpolicy&&(o.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?o.credentials="include":e.crossorigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function i(e){if(e.ep)return;e.ep=!0;const o=n(e);fetch(e.href,o)}})();const S="https://pixabay.com/api/",k="42057283-adafa6fc2ce046555d94b0faa";function f(t,r=1){return axios.get(`${S}`,{params:{key:k,q:t,per_page:12,lang:"en",page:r}}).then(({data:n})=>n)}const c="/goit-js-hw-012/assets/icons-abf892e8.svg";function g(t,r){const n=t.map(({webformatURL:i,largeImageURL:e,tags:o,likes:l,views:L,comments:v,downloads:b})=>`
+import{S as w,i as M}from"./assets/vendor-46aac873.js";import"https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js";(function(){const a=document.createElement("link").relList;if(a&&a.supports&&a.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const c of o.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&i(c)}).observe(document,{childList:!0,subtree:!0});function n(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerpolicy&&(o.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?o.credentials="include":e.crossorigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function i(e){if(e.ep)return;e.ep=!0;const o=n(e);fetch(e.href,o)}})();const S="https://pixabay.com/api/",k="42057283-adafa6fc2ce046555d94b0faa";function p(t,a=1){return axios.get(`${S}`,{params:{key:k,q:t,per_page:15,lang:"en",page:a}}).then(({data:n})=>n)}const l="/goit-js-hw-012/assets/icons-abf892e8.svg";function f(t,a){const n=t.map(({webformatURL:i,largeImageURL:e,tags:o,likes:c,views:L,comments:v,downloads:b})=>`
         <li class="gallery-item">
           <a class="gallery-link" href="${e}">
           <img
@@ -12,16 +12,16 @@ import{S as w,i as M}from"./assets/vendor-46aac873.js";import"https://cdn.jsdeli
             width="20"
             height="20"
             class="icon-in-block">
-            <use href="${c}#icon-heart"></use>
+            <use href="${l}#icon-heart"></use>
             </svg>
-            <span class="descr-span">${l}</span> 
+            <span class="descr-span">${c}</span> 
             </span>
             <span class="cont-in-p">
             <svg
             width="20"
             height="20"
             class="icon-in-block">
-            <use href="${c}#icon-eye"></use>
+            <use href="${l}#icon-eye"></use>
             </svg>
             <span class="descr-span">${L}</span> 
             </span>
@@ -30,7 +30,7 @@ import{S as w,i as M}from"./assets/vendor-46aac873.js";import"https://cdn.jsdeli
             width="20"
             height="20"
             class="icon-in-block">
-            <use href="${c}#icon-bubble2"></use>
+            <use href="${l}#icon-bubble2"></use>
             </svg>
             <span class="descr-span">${v}</span> 
             </span>
@@ -39,11 +39,11 @@ import{S as w,i as M}from"./assets/vendor-46aac873.js";import"https://cdn.jsdeli
             width="20"
             height="20"
             class="icon-in-block">
-            <use href="${c}#icon-arrow-down"></use>
+            <use href="${l}#icon-arrow-down"></use>
             </svg>
             <span class="descr-span">${b}</span>
             </span>
             </p>
           </a>
-        </li>`).join("");r.insertAdjacentHTML("beforeend",n)}let m=document.querySelector(".loading");const s={form:document.querySelector(".form"),input:document.querySelector(".form-input"),gallery:document.querySelector(".gallery"),searchBtn:document.querySelector(".search-button"),loadMoreBtn:document.querySelector(".dead-end-btn"),waiter:document.querySelector(".please-wait"),noMore:document.querySelector(".no-more")},a="is-hidden";let d=1,u="",p=0,$=12;function q(t){M.show({position:"topLeft",message:t,backgroundColor:"#EF4040",messageColor:"#FAFAFB",messageSize:"16px",close:!1,closeOnClick:!0,closeOnEscape:!0})}const h=new w(".gallery-item a",{captionsData:"alt",captionDelay:250});s.form.addEventListener("submit",A);function B(){setTimeout(()=>{m.classList.add("is-hidden")},500)}function E(){m.classList.remove("is-hidden")}async function A(t){t.preventDefault(),s.gallery.innerHTML="",E();const r=t.currentTarget;u=r.elements.input.value.trim();try{const{hits:n,totalHits:i}=await f(u);p=Math.ceil(i/$),console.log(p),B(),g(n,s.gallery),h.refresh(),n.length>0&n.length!==i?(s.loadMoreBtn.classList.remove(a),s.loadMoreBtn.addEventListener("click",y)):(s.loadMoreBtn.classList.add(a),q("Sorry, incorrect input value"))}catch(n){console.error(n)}finally{r.reset()}}async function y(){s.loadMoreBtn.classList.add(a),s.waiter.classList.remove(a),d+=1;try{const{hits:t}=await f(u,d);g(t,s.gallery),h.refresh(),s.waiter.classList.add(a)}catch(t){console.log(t)}finally{s.loadMoreBtn.classList.remove(a),d===p&&(s.loadMoreBtn.classList.add(a),s.loadMoreBtn.removeEventListener("click",y),s.noMore.classList.remove(a))}}
+        </li>`).join("");a.insertAdjacentHTML("beforeend",n)}let g=document.querySelector(".loading");const s={form:document.querySelector(".form"),input:document.querySelector(".form-input"),gallery:document.querySelector(".gallery"),searchBtn:document.querySelector(".search-button"),loadMoreBtn:document.querySelector(".dead-end-btn"),waiter:document.querySelector(".please-wait"),noMore:document.querySelector(".no-more")},r="is-hidden";let d=1,u="",m=0,$=15;function q(t){M.show({position:"topLeft",message:t,backgroundColor:"#EF4040",messageColor:"#FAFAFB",messageSize:"16px",close:!1,closeOnClick:!0,closeOnEscape:!0})}const h=new w(".gallery-item a",{captionsData:"alt",captionDelay:250});s.form.addEventListener("submit",A);function B(){setTimeout(()=>{g.classList.add(r)},500)}function E(){g.classList.remove(r)}async function A(t){t.preventDefault(),s.gallery.innerHTML="",E();const a=t.currentTarget;u=a.elements.input.value.trim(),s.noMore.classList.add(r);try{const{hits:n,totalHits:i}=await p(u);m=Math.ceil(i/$),B(),f(n,s.gallery),h.refresh(),n.length>0&&n.length!==i?(await s.loadMoreBtn.classList.remove(r),s.loadMoreBtn.addEventListener("click",y)):(s.loadMoreBtn.classList.add(r),q("Sorry, incorrect input value"))}catch(n){console.error(n)}finally{a.reset()}}async function y(){s.loadMoreBtn.classList.add(r),s.waiter.classList.remove(r),d+=1;try{const{hits:t}=await p(u,d);f(t,s.gallery),h.refresh(),s.waiter.classList.add(r)}catch(t){console.log(t)}finally{s.loadMoreBtn.classList.remove(r),d===m&&(s.loadMoreBtn.classList.add(r),s.loadMoreBtn.removeEventListener("click",y),s.noMore.classList.remove(r))}}
 //# sourceMappingURL=commonHelpers.js.map
